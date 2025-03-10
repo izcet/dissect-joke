3dc292bda447b38d4d45ee1a001186234a8097fd
Isaac Rhett <izcet@users.noreply.github.com>
2025-03-10 18:32:04 -0400

    Initial Commit

---
+++ generate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# a b aa ab ba bb aaa [...] bbbbbba bbbbbbb
+BABBLE="$(\
+  FOO="" ; \
+  for i in {1..7} ; do \
+    FOO="$FOO"'{a..b}' ; \
+    BAR={,$FOO}; \
+    bash -c 'echo "echo '$BAR'"' | bash ; \
+  done)"
+
+mkdir -p "out"
+
+
+for I in $BABBLE ; do 
+  DIR="out/$I"
+  mkdir -p "$DIR"
+  echo -e "#include <stdio.h>\n\nint main () {\n  int x = 5;\n  printf(" > "$DIR/$I.c"
+  echo "    \"$I\n\"," | sed 's/[ab]/%d /g' >> "$DIR/$I.c"
+  echo "    $I" | sed 's/a/x++, /g' | sed 's/b/++x, /g' | sed 's/, $//' >> "$DIR/$I.c"
+  echo -e "  );\n  return 0;\n}\n" >> "$DIR/$I.c"
+
+  #cat "$DIR/$I.c"
+
+  gcc "$DIR/$I.c" -o "$DIR/gcc-$I.bin" 2> /dev/null
+  clang "$DIR/$I.c" -o "$DIR/clang-$I.bin" 2> /dev/null
+
+  "./$DIR/gcc-$I.bin" > "$DIR/gcc-$I.out"
+  "./$DIR/clang-$I.bin" > "$DIR/clang-$I.out"
+
+  # aligning text the cheap way
+  PADDING="         "  
+  PADDED="$I$PADDING"
+  PADDED="${PADDED:0:10}"
+
+  echo "$PADDED" `cat "$DIR/gcc-$I.out"` >> gcc.out
+  echo "$PADDED" `cat "$DIR/clang-$I.out"` >> clang.out
+
+done
